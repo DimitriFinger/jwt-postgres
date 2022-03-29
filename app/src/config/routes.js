e71ea281c53f38efb406e136a5833b48e4a18b15
Dimitri <dimitri.finger@outlook.com>
2022-03-28 23:33:16 -0300

    Routes and first files created

---
+++ app/src/config/routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate
+} from 'react-router-dom';
+
+import HomePage from '../pages/Home/HomePage';
+import LoginPage from '../pages/Login/LoginPage';
+import WelcomePage from '../pages/Welcome/WelcomePage';
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path='/welcome' element={<WelcomePage />} />
+                <Route exact path='/login' element={<LoginPage />} />
+                <Route exact path='/home' element={<HomePage />} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRoutes
