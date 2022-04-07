cda1dcee55610eddbe08e11d252d2a708b4e9e17
Dimitri <dimitri.finger@outlook.com>
2022-04-07 00:11:14 -0300

    Docker working

--- server/src/database/knexfile.js
+++ server/src/database/knexfile.js
@@ -4,11 +4,11 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.DB_HOST || '127.0.0.1',
-      port: process.env.DB_PORT || '5432',
-      user: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASSWORD || 'd1m3w4096',
-      database: process.env.DB_NAME || 'aws-app'
+      host: 'db',
+      port: '5432',
+      user: 'postgres_user',
+      password: 'postgres_pass',
+      database: 'postgres_db'
     },
     pool: {
       min: 2,
